21060367cbacfd34eff0dab0f4a5a4a8bf2c8b13
Clecius J. Martinkoski <clecius@e-precise.com.br>
2017-07-03 01:29:26 -0300

    Ajustado script para preencher o database

--- downloadPages.sh
+++ downloadPages.sh
@@ -4,12 +4,23 @@ pastaParaSites=pages
 
 arquivoIndices=links.txt
 
+cacheManagerDir=cache-manager
+
+cacheManagerBin=$cacheManagerDir/bin/cache-manager
+
+cacheDb=./cache.db
+cacheIndex=./cache.idx
+
 rm -rf $pastaParaSites;
 
 mkdir -p $pastaParaSites;
+
+make -C $cacheManagerDir;
 
 while read p; do
   filename=$(printf '%s' $p | md5sum | cut -d ' ' -f 1)
   echo "Baixando: $p em $filename";
   curl $p > "$pastaParaSites/$filename.html";
+  $cacheManagerBin -d $cacheDb -t $cacheIndex -i -k $p -I "$pastaParaSites/$filename.html"
+
 done < $arquivoIndices
